14b5090a6b86709f436de25d41db285f6a045dc7
adzarei <adzarei@inf.upv.es>
2018-10-29 01:47:35 +0100

    Connect to MySQL v8 DB DEV/PROD connection properties

--- src/main/java/guru/springfamework/bootstrap/Bootstrap.java
+++ src/main/java/guru/springfamework/bootstrap/Bootstrap.java
@@ -24,8 +24,8 @@ public class Bootstrap implements CommandLineRunner{
     @Override
     public void run(String... args) throws Exception {
 
-        loadCategroyRepository();
-        loadCustomerRepository();
+        //loadCategroyRepository();
+        //loadCustomerRepository();
 
     }
 
